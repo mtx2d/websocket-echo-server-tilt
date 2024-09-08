f7d7baf92db1b78049794a8ca22e0fc84967b811
Michael <mtx2d@outlook.com>
2024-09-08 15:03:24 -0700

    map port to 80 sovles the problem

--- echo-server/server.py
+++ echo-server/server.py
@@ -6,7 +6,7 @@ async def echo(websocket, path):
     async for message in websocket:
         await websocket.send(message)
 
-start_server = websockets.serve(echo, "0.0.0.0", 80)
+start_server = websockets.serve(echo, "0.0.0.0", 8089)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
